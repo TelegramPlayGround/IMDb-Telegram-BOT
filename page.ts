7454162058afe54dd55d970956579766a2b62ee3
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2024-02-01 20:28:15 +0530

    Fixes for fetch

--- page.ts
+++ page.ts
@@ -1,12 +1,12 @@
 export async function urlFetchEr(
     webPageUrl: string,
     // https://www.scaler.com/topics/typescript/typescript-dictionary/
-    customHeaders: {[key: string]: string},
-    preFetchOnly?: boolean
+    customHeaders: { [key: string]: string },
+    preFetchOnly?: boolean,
 ): Promise<string> {
-    customHeaders["User-Agent"] = (Deno.env.get("BROWSER_USER_AGENT") as string);
+    customHeaders["User-Agent"] = Deno.env.get("BROWSER_USER_AGENT") as string;
     const one = await fetch(webPageUrl, {
-        headers: customHeaders
+        headers: customHeaders,
     });
     if (preFetchOnly === true) {
         // @ts-ignore
