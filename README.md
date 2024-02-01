1bdd8dbcf1f329a7bed154bbef37a9d577c242c1
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2024-02-01 20:36:11 +0530

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Rename the `.env.sample` file to `.env`
 
 Run the `deno task start`
+
+## Why `MtKruto` for this simple Telegram Bot ?
+
+- we can avoid various [BOT API workarounds](https://t.me/BotTalk/685273) required to make the bot run on the different [Telegram Applications](https://github.com/telegramdesktop/tdesktop/issues/10527).
--- page.ts
+++ page.ts
@@ -1,17 +1,17 @@
 export async function urlFetchEr(
-    webPageUrl: string,
-    // https://www.scaler.com/topics/typescript/typescript-dictionary/
-    customHeaders: { [key: string]: string },
-    preFetchOnly?: boolean,
+  webPageUrl: string,
+  // https://www.scaler.com/topics/typescript/typescript-dictionary/
+  customHeaders: { [key: string]: string },
+  preFetchOnly?: boolean,
 ): Promise<string> {
-    customHeaders["User-Agent"] = Deno.env.get("BROWSER_USER_AGENT") as string;
-    const one = await fetch(webPageUrl, {
-        headers: customHeaders,
-    });
-    if (preFetchOnly === true) {
-        // @ts-ignore
-        return one;
-    }
-    const two = await one.text();
-    return two;
+  customHeaders["User-Agent"] = Deno.env.get("BROWSER_USER_AGENT") as string;
+  const one = await fetch(webPageUrl, {
+    headers: customHeaders,
+  });
+  if (preFetchOnly === true) {
+    // @ts-ignore
+    return one;
+  }
+  const two = await one.text();
+  return two;
 }
